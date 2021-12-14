8308d572201d4465a801793141b6411e65a0f9ae
Hans Donner <hans@hansdonner.nl>
2021-12-14 08:15:35 +0100

    font-ia-writer-variable

---
+++ Casks/font-ia-writer-variable.rb
@@ -0,0 +1,15 @@
+cask "font-ia-writer-variable" do
+  version :latest
+  sha256 :no_check
+
+  url "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+  name "iA Writer Variable"
+  homepage "https://github.com/iaolo/iA-Fonts"
+
+  font "iA-Fonts-master/iA Writer Duo/Variable/iAWriterDuoV.ttf"
+  font "iA-Fonts-master/iA Writer Duo/Variable/iAWriterDuoV-Italic.ttf"
+  font "iA-Fonts-master/iA Writer Mono/Variable/iAWriterMonoV.ttf"
+  font "iA-Fonts-master/iA Writer Mono/Variable/iAWriterMonoV-Italic.ttf"
+  font "iA-Fonts-master/iA Writer Quattro/Variable/iAWriterQuattroV.ttf"
+  font "iA-Fonts-master/iA Writer Quattro/Variable/iAWriterQuattroV-Italic.ttf"
+end
